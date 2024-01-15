0204eec7d65a45a6915fb9c5a9fe20361a82422a
Tanvi Khadpe <khadpe.tanvi@gmail.com>
2024-01-15 19:56:17 +0530

    Manage members completed
    Manage members integration completed

--- discord-clone/app/api/members/[memberId]/route.ts
+++ discord-clone/app/api/members/[memberId]/route.ts
@@ -1,4 +1,5 @@
 import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
@@ -12,8 +13,8 @@ export async function PATCH(
 
         const serverId = searchParams.get("serverId");
         
-        if (!serverId){
-            return new NextResponse("Server ID Missing", { status: 400 })
+        if (!profile){
+            return new NextResponse("Unauthorized", { status: 401 })
         }
 
         if (!serverId){
@@ -23,8 +24,97 @@ export async function PATCH(
         if (!params.memberId){
             return new NextResponse("Member ID Missing", { status: 400 })
         }
+
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                profileId: profile.id,
+            },
+            data: {
+                members:{
+                    update: {
+                        where: {
+                            id: params.memberId,
+                            profileId: {
+                                not: profile.id
+                            }
+                        },
+                        data: {
+                            role
+                        }
+                    }
+                }
+            },
+            include: {
+                members: {
+                    include: {
+                        profile: true,
+                    },
+                    orderBy: {
+                        role: "asc"
+                    }
+                }
+            }
+        });
+
+        return NextResponse.json(server);
+
     } catch (error){
         console.log("[MEMBERS_ID_PATCH]", error);
         return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { memberId: string } }
+){
+    try{
+        const profile = await currentProfile();
+        const { searchParams } = new URL(req.url);
+        const serverId = searchParams.get("serverId");
+
+        if(!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if(!serverId) {
+            return new NextResponse("Server ID Missing", { status: 400 });
+        }
+
+        if(!params.memberId) {
+            return new NextResponse("Member ID Missing", { status: 400 });
+        }
+
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                profileId: profile.id,
+            },
+            data:{
+                members: {
+                    deleteMany: {
+                        id: params.memberId,
+                        profileId: {
+                            not: profile.id
+                        }
+                    }
+                }
+            },
+            include:{
+                members: {
+                    include: {
+                        profile: true,
+                    },
+                    orderBy: {
+                        role: "asc"
+                    },
+                },
+            },
+        });
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log("[MEMBER_ID_DELETE]", error);
+        return new NextResponse("Internal Error", { status: 500 });
     }
 }
